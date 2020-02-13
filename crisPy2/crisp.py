7b16112b238a652a2fddb59330f72aed7f44915d
John Armstrong <j.armstrong.2@research.gla.ac.uk>
2020-02-13 12:39:21 +0000

    "Updated crisp.py"

--- crisPy2/crisp.py
+++ crisPy2/crisp.py
@@ -38,98 +38,154 @@ class CRISP:
     '''
 
     def __init__(self, files, align=False):
-        self.mm_sun = 1391 * u.megameter
-        self.ang_sun = 1920 * u.arcsec
+        self.mm_sun = 1391 << u.megameter
+        self.ang_sun = 1920 << u.arcsec
         if type(files) == str:
             if ".fits" in files:
                 if "ca" in files:
                     self.ca = fits.open(files)[0]
                     try:
-                        self.ca_wvls = self.ca.header["spect_pos"]
+                        self.ca_wvls = self.ca.header["spect_pos"] << u.Angstrom
                     except KeyError:
-                        self.ca_wvls = fits.open(files)[1].data
+                        self.ca_wvls = fits.open(files)[1].data << u.Angstrom
+                    self.px_res = self.ca.header["CDELT1"] << u.arcsec / u.pixel
                 else:
                     self.ha = fits.open(files)[0]
                     try:
-                        self.ha_wvls = self.ha.header["spect_pos"]
+                        self.ha_wvls = self.ha.header["spect_pos"] << u.Angstrom
                     except KeyError:
-                        self.ha_wvls = fits.open(files)[1].data
+                        self.ha_wvls = fits.open(files)[1].data << u.Angstrom
+                    self.px_res = self.ha.header["CDELT1"] << u.arcsec / u.pixel
             elif ".h5" or ".hdf5" in files:
                 if "ca" in files:
                     ca = h5py.File(files, "r")
                     self.ca = ObjDict({})
                     self.ca["data"] = ca["data"]
                     self.ca["header"] = yaml.load(ca["header"][0], Loader=yaml.Loader)
-                    self.ca_wvls = self.ca.header["spect_pos"]
+                    self.ca_wvls = self.ca.header["spect_pos"] << u.Angstrom
+                    self.px_res = self.ca.header["pixel_scale"] << u.arcsec / u.pixel
                 else:
                     ha = h5py.File(files, "r")
                     self.ha = ObjDict({})
                     self.ha["data"] = ha["data"]
                     self.ha["header"] = yaml.load(ha["header"][0], Loader=yaml.Loader)
-                    self.ha_wvls = self.ha.header["spect_pos"]
+                    self.ha_wvls = self.ha.header["spect_pos"] << u.Angstrom
+                    self.px_res = self.ha.header["pixel_scale"] << u.arcsec / u.pixel
         else:
             for f in files:
                 if ".fits" in f:
                     if "ca" in f:
                         self.ca = fits.open(f)[0]
                         try:
-                            self.ca_wvls = self.ca.header["spect_pos"]
+                            self.ca_wvls = self.ca.header["spect_pos"] << u.Angstrom
                         except KeyError:
-                            self.ca_wvls = fits.open(f)[1].data
+                            self.ca_wvls = fits.open(f)[1].data << u.Angstrom
+                        self.px_res = self.ca.header["CDELT1"] << u.arcsec / u.pixel
                     else:
                         self.ha = fits.open(f)[0]
                         try:
-                            self.ha_wvls = self.ha.header["spect_pos"]
+                            self.ha_wvls = self.ha.header["spect_pos"] << u.Angstrom
                         except KeyError:
-                            self.ha_wvls = fits.open(f)[1].data
+                            self.ha_wvls = fits.open(f)[1].data << u.Angstrom
                 elif ".h5" or ".hdf5" in f:
                     if "ca" in f:
                         ca = h5py.File(f, "r")
                         self.ca = ObjDict({})
                         self.ca["data"] = ca["data"]
                         self.ca["header"] = yaml.load(ca["header"][0], Loader=yaml.Loader)
-                        self.ca_wvls = self.ca.header["spect_pos"]
+                        self.ca_wvls = self.ca.header["spect_pos"] << u.Angstrom
+                        self.px_res = self.ca.header["pixel_scale"] << u.arcsec / u.pixel
                     else:
                         ha = h5py.File(f, "r")
                         self.ha = ObjDict({})
                         self.ha["data"] = ha["data"]
                         self.ha["header"] = yaml.load(ha["header"][0], Loader=yaml.Loader)
-                        self.ha_wvls = self.ha.header["spect_pos"]
+                        self.ha_wvls = self.ha.header["spect_pos"] << u.Angstrom
 
     def __str__(self):
         if "ca" and "ha" in self.__dict__:
-            date = self.ca.header["DATE-AVG"][:10]
-            time = self.ca.header["DATE-AVG"][11:-4]
-            el_1 = self.ca.header["WDESC1"]
-            samp_wvl_1 = str(self.ca.header["NAXIS3"])
-            el_2 = self.ha.header["WDESC1"]
-            samp_wvl_2 = str(self.ha.header["NAXIS3"])
-            pointing = (str(round(self.ca.header["CRVAL1"],3)), str(round(self.ca.header["CRVAL2"],3)))
+            try:
+                date = self.ca.header["DATE-AVG"][:10]
+                time = self.ca.header["DATE-AVG"][11:-4]
+                el_1 = self.ca.header["WDESC1"]
+                samp_wvl_1 = str(self.ca.header["NAXIS3"])
+                el_2 = self.ha.header["WDESC1"]
+                samp_wvl_2 = str(self.ha.header["NAXIS3"])
+                pointing = (str(round(self.ca.header["CRVAL1"],3)), str(round(self.ca.header["CRVAL2"],3)))
+            except KeyError: # must be using hdf5 data files
+                date = self.ca.header["date-obs"]
+                time = self.ca.header["time-obs"]
+                el_1 = self.ca.header["element"]
+                samp_wvl_1 = str(self.ca.header["dimensions"][-3])
+                el_2 = self.ha.header["element"]
+                samp_wvl_2 = str(self.ha.header["dimensions"][-3])
+                pointing = (str(self.ca.header["crval"][-1]), str(self.ca.header["crval"][-2]))
             if len(self.ca.data.shape) == 3:
                 return f"CRISP observation from {date} {time} UTC with measurements taken in the elements {el_1} angstroms and {el_2} angstroms with {samp_wvl_1} and {samp_wvl_2} wavelengths sampled, respectively. Heliocentric coodinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. Only Stokes I present in these observations."
             elif len(self.ca.data.shape) == 4:
                return f"CRISP observation from {date} {time} UTC with measurements taken in the elements {el_1} angstroms and {el_2} angstroms with {samp_wvl_1} and {samp_wvl_2} wavelengths sampled, respectively. Heliocentric coodinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. All Stokes parameters present in these observations."
         elif "ca" and not "ha" in self.__dict__:
-            date = self.ca.header["DATE-AVG"][:10]
-            time = self.ca.header["DATE-AVG"][11:-4]
-            el_1 = self.ca.header["WDESC1"]
-            samp_wvl_1 = str(self.ca.header["NAXIS3"])
-            pointing = (str(round(self.ca.header["CRVAL1"],3)), str(round(self.ca.header["CRVAL2"],3)))
+            try:
+                date = self.ca.header["DATE-AVG"][:10]
+                time = self.ca.header["DATE-AVG"][11:-4]
+                el_1 = self.ca.header["WDESC1"]
+                samp_wvl_1 = str(self.ca.header["NAXIS3"])
+                pointing = (str(round(self.ca.header["CRVAL1"],3)), str(round(self.ca.header["CRVAL2"],3)))
+            except KeyError:
+                date = self.ca.header["date-obs"]
+                time = self.ca.header["time-obs"]
+                el_1 = self.ca.header["element"]
+                samp_wvl_1 = str(self.ca.header["dimensions"][-3])
+                pointing = (str(self.ca.header["crval"][-1]), str(self.ca.header["crval"][-2]))
             if len(self.ca.data.shape) == 3:
                 return f"CRISP observation from {date} {time} UTC with measurements taken in the elements {el_1} angstroms with {samp_wvl_1} wavelengths sampled and with heliocentric coordinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. Only Stokes I present in this observation."
             elif len(self.ca.data.shape) == 4:
                 return f"CRISP observation from {date} {time} UTC with measurements taken in the elements {el_1} angstroms with {samp_wvl_1} wavelengths sampled and with heliocentric coordinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. All Stokes parameters present in these observations."
         elif not "ca" and "ha" in self.__dict__:
-            date = self.ha.header["DATE-AVG"][:10]
-            time = self.ha.header["DATE-AVG"][11:-4]
-            el_1 = self.ha.header["WDESC1"]
-            samp_wvl_1 = str(self.ha.header["NAXIS3"])
-            pointing = (str(round(self.ha.header["CRVAL1"],3)), str(round(self.ha.header["CRVAL2"],3)))
+            try:
+                date = self.ha.header["DATE-AVG"][:10]
+                time = self.ha.header["DATE-AVG"][11:-4]
+                el_1 = self.ha.header["WDESC1"]
+                samp_wvl_1 = str(self.ha.header["NAXIS3"])
+                pointing = (str(round(self.ha.header["CRVAL1"],3)), str(round(self.ha.header["CRVAL2"],3)))
+            except KeyError:
+                date = self.ha.header["date-obs"]
+                time = self.ha.header["time-obs"]
+                el_1 = self.ha.header["element"]
+                samp_wvl_1 = str(self.ha.header["dimensions"][-3])
+                pointing = (str(self.ha.header["crval"][-1]), str(self.ha.header["crval"][-2]))
             if len(self.ha.data.shape) == 3:
                 return f"CRISP observation from {date} {time} UTC with measurements taken in the element {el_1} angstroms with {samp_wvl_1} wavelengths sampled and with heliocentric coordinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. Only Stokes I present in this observation."
             elif len(self.ha.data.shape) == 4:
                 return f"CRISP observation from {date} {time} UTC with measurements taken in the element {el_1} angstroms with {samp_wvl_1} wavelengths sampled and with heliocentric coordinates at the centre of the image ({pointing[0]},{pointing[1]}) in arcseconds. All Stokes parameters present in these observations."
 
-    @staticmethod
-    def unit_conversion(coord, unit_to, centre=False):
-        
+    def unit_conversion(self, coord, unit_to, centre=False):
+       '''
+       A method to convert unit coordinates between pixel number, arcsecond (either absolute or relative to pointing) and megametre.
+
+       Parameters
+       ----------
+       coord : tuple or list of astropy.Quantities
+           The coordinate to be transformed.
+       unit_to : str
+           The coordinate system to convert to.
+       centre : bool, optional
+           Whether or not to calculate the pixel in arcseconds with respect to the pointing e.g. in the helioprojective frame.
+       '''
+
+       if not centre:
+           if unit_to == "pix":
+               if coord.unit == "pix":
+                   return coord
+               elif coord.unit == "arcsec":
+                   return np.round(coord / self.px_res)
+               elif coord.unit == "megameter":
+                   return np.round((coord * self.ang_sun / self.mm_sun) / self.px_res) 
+           elif unit_to == "arcsec":
+               if coord.unit == "pix":
+                   return coord * self.px_res
+               elif coord.unit == "arcsec":
+                   return coord
+               elif coord.unit == "megameter":
+                   return coord * (self.ang_sun / self.mm_sun)
+           elif unit_to == "Mm":
