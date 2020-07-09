0a3931f4bd47ee9866e6f86f35ba16bbe29492b2
John Armstrong <j.armstrong.2@research.gla.ac.uk>
2020-07-09 19:21:04 +0100

    Added some new docs.

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 
@@ -47,8 +48,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
-# html_theme_path = sunpy_sphinx_theme.get_html_theme_path()
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
