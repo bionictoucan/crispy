b84482bba7ead44a26576c4a7f6c5ee4d4392809
rhero12 <jarmstrong1210@outlook.com>
2021-08-25 16:31:01 +0100

    Updated __init__.py.

--- crispy/__init__.py
+++ crispy/__init__.py
@@ -1,9 +1,3 @@
 from .crisp import *
-from .utils import *
 from .inversions import *
-from .io import *
-from .mixin import *
-from .neural_network import *
-from .spectral import *
-from .visualisation import *
 __version__ = "1.0.0"
