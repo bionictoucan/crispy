5dff8db20db0b0d94627d2c3a5aca62961d0e138
bionictoucan <jarmstrong1210@outlook.com>
2022-03-16 14:41:25 +0000

    Added docs to main branch.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bionictoucan/crispy",
-    packages=["crispy"],
+    packages=setuptools.find_packages(),
     install_requires = [
         "numpy <1.22, >1.19",
         "astropy",
