Updated rotation code, correctly works better with headers, and now rotates the WCS so can still plot in helioprojective. Number of minor bugs fixed

crispy/crisp.py
@@ -262,6 +262,23 @@ class CRISP(CRISPSlicingMixin):
         crop, crop_dict = rotate_crop_data(self.data)
         for k, v in crop_dict.items():
             self.file.header[k] = v
+        if not isinstance(self.file.header["frame_dims"], list):
+            self.file.header["frame_dims"] = repr(crop_dict["frame_dims"])
+        a = -crop_dict["angle"]
+        c = np.cos(a)
+        s = np.sin(a)
+        self.file.header["PC1_1"] = c
+        self.file.header["PC1_2"] = -s
+        self.file.header["PC2_1"] = s
+        self.file.header["PC2_2"] = c
+        self.file.header["CRPIX1"] -= crop_dict["x_min"]
+        self.file.header["CRPIX2"] -= crop_dict["y_min"]
+        # NOTE(cmo): WCS Seems to complain about the formatting of comment sometimes, so just squash it
+        self.file.header.pop("COMMENT", None)
+        try:
+            self.wcs = WCS(self.file.header)
+        except ValueError:
+            self.wcs = zarr_header_to_wcs(self.file.header)
         self.file.data = crop
         self.rotate = True
         return None
@@ -288,15 +305,29 @@ class CRISP(CRISPSlicingMixin):
 
         assert "frame_dims" in self.header
         crop_dict = {
-            k: self.header[k]
+            k: self.file.header[k]
             for k in ["frame_dims", "x_min", "x_max", "y_min", "y_max", "angle"]
         }
+        if isinstance(crop_dict["frame_dims"], str):
+            crop_dict["frame_dims"] = [int(x) for x in crop_dict["frame_dims"][1:-1].split(',')]
 
         if sep:
             return reconstruct_full_frame(crop_dict, self.data)
 
         self.rot_data = self.data
         self.file.data = reconstruct_full_frame(crop_dict, self.data)
+        self.file.header.pop("PC1_1", None)
+        self.file.header.pop("PC1_2", None)
+        self.file.header.pop("PC2_1", None)
+        self.file.header.pop("PC2_2", None)
+        self.file.header["CRPIX1"] += crop_dict["x_min"]
+        self.file.header["CRPIX2"] += crop_dict["y_min"]
+        # NOTE(cmo): WCS Seems to complain about the formatting of comment sometimes, so just squash it
+        self.file.header.pop("COMMENT", None)
+        try:
+            self.wcs = WCS(self.file.header)
+        except ValueError:
+            self.wcs = zarr_header_to_wcs(self.file.header)
         self.rotate = False
         return None
 

crispy/utils.py
@@ -528,7 +528,7 @@ def rotate_crop_data(im):
     if len(im.shape) == 4:
         stokes = True
         midWvl = im.shape[1] // 2
-        imCore = np.copy(im[midWvl])
+        imCore = np.copy(im[0, midWvl])
         im = im.reshape(-1, *im.shape[-2:])
     elif len(im.shape) == 3:
         midWvl = im.shape[0] // 2
@@ -582,7 +582,7 @@ def rotate_crop_data(im):
         :, cropData["y_min"] : cropData["y_max"], cropData["x_min"] : cropData["x_max"]
     ]
     if stokes:
-        crop = crop.reshape(4, -1, crop.shape[-2:])
+        crop = crop.reshape(4, -1, *crop.shape[-2:])
     return crop, cropData
 
 